1e982485b349a57518f3dcb99ca91765b8d938b4
Mijat Krivokapić <mijat2004@gmail.com>
2024-01-11 18:51:36 +0100

    rename edge coefficient

---
+++ antcolony/ant_colony_optimization.py
@@ -0,0 +1,15 @@
+class AntColonyOptimization:
+    def __init__(self) -> None:
+        pass
+    """
+        m - number of ants
+        alpha - influence of pheromones on choice next node
+        beta - influence of distance on choice of next node
+        rho - speed of pheromone evaporation
+        source - first node of the path
+        destination - 
+    """
+    def find_shortest_path(graph,m,alpha,beta,rho,source,destination):
+    
+
+    
