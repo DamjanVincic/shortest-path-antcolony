a4ddbcaa80c7776d5c44f907a992ee002f30436d
Damjan Vincic <damjanvincic2003@gmail.com>
2024-01-11 19:46:26 +0100

    Add ant choose node

--- antcolony/ant.py
+++ antcolony/ant.py
@@ -12,3 +12,20 @@ class Ant:
         self.path = [source]
         self.path_length = 0
         self.visited_nodes = set(source)
+
+    def _choose_node(self):
+        unvisited_neighbours = self._get_unvisited_neighbours()
+        if len(unvisited_neighbours) == 0:
+            return None
+
+        edge_coefficients = {}
+        for neighbour in unvisited_neighbours[0]:
+            edge = self.nodes[self.current_node, neighbour]
+            edge_coefficients[neighbour] = utils.edge_coefficient(edge['pheromones'], edge['distance'], self.alpha, self.beta)
+
+        next_node = utils.roulette_selection(edge_coefficients)
+        return next_node
+
+    def _get_unvisited_neighbours(self):
+        return [node for node in self.nodes.get_neighbours(self.current_node) if node not in self.visited_nodes]
+
