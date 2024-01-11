b67cc6e3d084d85ecb8904eec1f0025737243232
Damjan Vincic <damjanvincic2003@gmail.com>
2024-01-11 16:49:16 +0100

    Add a few comments

--- antcolony/nodes.py
+++ antcolony/nodes.py
@@ -9,6 +9,15 @@ class Nodes:
 
     def _load_graph(self, filename):
         graph = nx.Graph()
+
+        r"""
+        (\d+) - capturing group for multiple digits
+        \( - the '(' character
+        (\d.+) - capturing group for multiple digits and other characters (in our case '.' for float numbers)
+        , - the ',' character
+        \) - the ')' character
+        (?::(.+))? - a non capturing group of a character ':' and a capturing group of any characters (for neighbour IDs)
+        """
         pattern = re.compile(r'(\d+)\((\d.+),(\d.+)\)(?::(.+))?')
         with open(filename, 'r') as f:
             for line in f.readlines():
@@ -17,6 +26,7 @@ class Nodes:
                 if not graph.has_node(node_id):
                     graph.add_node(node_id, data=Node(float(match.group(2)), float(match.group(3))))
                 elif not graph.nodes[node_id].get('data', None):
+                    # if node is in the graph but without its coordinates (if it was added as a neighbour to another one)
                     graph.nodes[node_id]['data'] = Node(float(match.group(2)), float(match.group(3)))
 
                 adjacent_nodes = match.group(4)
@@ -26,6 +36,8 @@ class Nodes:
                 for adjacent_node_id in adjacent_nodes.split(','):
                     if not graph.has_node(adjacent_node_id):
                         graph.add_node(adjacent_node_id)
+
+                    # If there isn't an edge between the node and the neighbour, and the neighbour has coordinates
                     if not graph.has_edge(node_id, adjacent_node_id) and graph.nodes[adjacent_node_id].get('data', None):
                         graph.add_edge(node_id, adjacent_node_id, distance=graph.nodes[node_id]['data'].distance(graph.nodes[adjacent_node_id]['data']))
         return graph
