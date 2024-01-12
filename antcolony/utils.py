58ff7395ad7ff442e26ec1917c2341f7e5eb8849
Mijat Krivokapić <mijat2004@gmail.com>
2024-01-12 19:12:57 +0100

    add comments

--- antcolony/utils.py
+++ antcolony/utils.py
@@ -11,16 +11,17 @@ def roulette_selection(coefficients: Dict[str, float]) -> str:
     total_coefficients = sum(coefficients[k] for k in coefficients)
     relative_coefficients = {}
 
+    #Calculate coefficients relative to the sum of all coefficients
     for k in coefficients:
         relative_coefficients[k] = coefficients[k]/total_coefficients
 
     current_sum = 0
-    cumulative_probability = {}
+    cumulative_probability = {} #Every node is assigned range proportional to its relative coefficient
     for k in coefficients:
         current_sum += relative_coefficients[k]
         cumulative_probability[k] = current_sum
 
-    rand = random.random()
+    rand = random.random() # Random number between 0 and 1
     for k in cumulative_probability:
-        if rand <= cumulative_probability[k]:
+        if rand <= cumulative_probability[k]: # Selected node is one in which's range random number falls
             return k
